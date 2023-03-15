1e06342153353f2659506fddfcc9a4de7c464492
All <all@apx10.com>
2023-03-15 12:13:38 +0100

    Testing out dev branch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='KaggleModule',
-    version='0.0.2',    
+    version='0.0.3',    
     description='All tools used for Anders\' kaggle work. Not intended to be used as is by anyone else. as of right now.',
     url='https://github.com/ahll19/Kaggle-Projects',
     author='Anders Højbak Lysgaard Lauridsen',
